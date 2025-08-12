757923dd25dbd6ed1f0ea36d7990051e2baa3ab3
Mohammadrasool Vahid <121235436+mutopia7@users.noreply.github.com>
2025-08-12 18:44:20 +0330

    debug: require crypto and add try catch for errors

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,9 +1,9 @@
 // messageController.js
-
+const crypto = require("crypto")
 const messages = require("../models/messageModel");
 
 const controller = {
-    render: (req, res) => {res.render( "layouts/layout", {messages : messages})},
+    render: (req, res) => { res.render("layouts/layout", { messages: messages }) },
 
     detailRender: (req, res) => {
         const id = req.params.messageId;
@@ -16,16 +16,23 @@ const controller = {
         res.render("layouts/messageDetail", { message });
     },
 
-    formRender: (req,res) => {
+    formRender: (req, res) => {
         res.render("layouts/form");
     },
 
-    postForm: (req,res) => {
-        const user = req.body.user;
-        const text = req.body.text;
-        const bgColor = req.body.bgColor;
-        messages.push({user,text,bgColor, id: crypto.randomUUID(), date: new Date()});
-        res.redirect("/")
+    postForm: (req, res) => {
+        try {
+            const user = req.body.user;
+            const text = req.body.text;
+            const bgColor = req.body.bgColor;
+            messages.push({ user, text, bgColor, id: crypto.randomUUID(), date: new Date() });
+            res.redirect("/")
+        } catch (err) {
+            console.error("Error in postForm:", err);
+            res.status(500).send("Internal Server Error");
+        }
+
+
     }
 };
 
